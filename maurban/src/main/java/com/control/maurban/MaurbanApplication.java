635d67b3d42c7824ebade16110c1881634642dc3
hebertberamos <hebertramos09@gmail.com>
2025-10-25 14:12:07 -0300

    Starting project

---
+++ maurban/src/main/java/com/control/maurban/MaurbanApplication.java
@@ -0,0 +1,13 @@
+package com.control.maurban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaurbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaurbanApplication.class, args);
+	}
+
+}
---
+++ maurban/src/test/java/com/control/maurban/MaurbanApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.control.maurban;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MaurbanApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
